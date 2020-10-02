ada62de5700b9584b5ba6fb6bacb21941d87bda3
NamhyeokJang <chrow16@naver.com>
2020-10-02 21:32:50 +0900

    기록:문제 필터링

--- frontend/src/pages/ViewRecords.js
+++ frontend/src/pages/ViewRecords.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
-import { Button } from 'antd'
+import { Button, Select } from 'antd'
 import moment from 'moment'
 import 'moment/locale/ko'
 import { FiCheckCircle, FiAlertCircle } from 'react-icons/fi'
@@ -11,6 +11,7 @@ import './ViewRecords.css'
 export default function ViewRecords() {
   const [logs, setLogs] = useState([])
   const [count, setCount] = useState(0)
+  const [select, setSelect] = useState('')
 
   useEffect(() => {
     fetchLogs().then(res => {
@@ -26,10 +27,19 @@ export default function ViewRecords() {
       </Helmet>
       <div className="view-records view container">
         <div className="view-records__top">
-          공부한 문제 수: {count}
+          <span>공부한 문제 수: {count}</span>
+          <Select defaultValue='' style={{ width: 120 }} onChange={setSelect}>
+            <Select.Option value=''>모든 문제</Select.Option>
+            <Select.Option value='fail'>틀린 문제</Select.Option>
+            <Select.Option value='success'>맞은 문제</Select.Option>
+          </Select>
         </div>
         <div className="view-records__logs-container">
-          {logs.map(log => <Log key={log.id} log={log} />)}
+          {logs.filter(log => {
+            if (!select) return true
+            else return log.type === select
+          }).map(log => <Log key={log.id} log={log} />)
+          }
         </div>
       </div>
     </>
